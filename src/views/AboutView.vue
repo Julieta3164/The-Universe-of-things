c808117f81802d2d8e93ff62d7f7c0a6e6ae38a6
Estherherrera <graxyherrera@gmail.com>
2022-07-08 09:34:29 +0200

    cambios editar borrar

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -12,129 +12,129 @@ export default {
     },
     methods: {
         ...mapActions(userFavoriteStore, ['addFavorite']),
-        remove(index){
-            this.favorite.splice(index,1)
+        remove(index) {
+            this.favorite.splice(index, 1)
         },
-        edit(character){
-            character.editing = false;
-            
+        edit(character) {
+            character.editing = false
         },
-        modify(character, user){
-            character.name = user;
-            character.editing = true;
+        modify(character, user) {
+            character.name = user
+            character.editing = true
             console.log(user)
             this.user = ' '
-            
         },
-        Cancel(character){
-            character.editing = true;
-        }
+        Cancel(character) {
+            character.editing = true
+        },
     },
-
 }
 </script>
 <template>
-<main class="fondo">
-    <div
-        v-for="(character, index) in favorite"
-        :key="index"
-        :id="'card-' + character._id"
-<<<<<<< HEAD
-        class="card col-6 card m-3"
-        style="width: 18rem"
-    >
-        <button name="tarjetaTest" v-on:click="this.like(character)"></button>
-        <button @click="remove(index)">delete</button>
-        <button @click="edit(character)" v-if="character.editing">Editar</button>
-        <button @click="Cancel(character)" v-else>Cancelar</button>
-
-        <img :src="character.imageUrl" class="card-img-top" alt="..." />
-=======
+    <main class="fondo">
+        <div
+            v-for="(character, index) in favorite"
+            :key="index"
+            :id="'card-' + character._id"
+            class="card col-6 card m-3 central"
+            style="width: 18rem"
+        >
+            <div class="icons">
+                <i @click="remove(index)" class="bi bi-trash"></i>
+                <!-- <button @click="remove(index)">delete</button> -->
+                <i  @click="edit(character)" v-if="character.editing" class="bi bi-pencil"></i>
+                <!-- <button @click="edit(character)" v-if="character.editing">
+                    Editar
+                </button> -->
+                <button @click="Cancel(character)" v-else>Cancelar</button>
+            </div>
+            <!-- <img :src="character.imageUrl" class="card-img-top" alt="..." />
         class="card col-6 card m-3 central"
-        style="width: 18rem">
-        <div class="icons">
-            <i class="bi bi-pencil"></i>
-            <i class="bi bi-trash"></i>
+        style="width: 18rem"> -->
 
+            
+            
+
+            <div class="img-card">
+                <img :src="character.imageUrl" class="card-img-top" alt="..." />
+            </div>
+            <div class="stars"></div>
+            <div class="card-body">
+                <h3 class="card-text" v-if="character.editing">
+                    {{ character.name }}
+                </h3>
+                <input
+                    type="text"
+                    v-model="user"
+                    v-else
+                    @keyup.enter="modify(character, user)"
+                    autofocus
+                />
+            </div>
         </div>
-        <div class="img-card">
-            <img :src="character.imageUrl" class="card-img-top" alt="..." />
-        </div>
-        <div class="stars">HOLA</div> 
->>>>>>> 895b609a7e1700c1c7a0e4b955b304fd9d2c6759
-        <div class="card-body">
-            <h3 class="card-text" v-if="character.editing" >{{ character.name }}</h3>
-            <input type="text" v-model="user" v-else @keyup.enter="modify(character, user)" autofocus/>
-        </div>
-    </div>
     </main>
 </template>
 
 <style lang="scss" scoped>
-.fondo{
-display: flex;
-  flex-wrap:wrap ;
-  margin-top: 1%;
-  background: #87cefa;
-  justify-content: center;
+.fondo {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 1%;
+    background: #87cefa;
+    justify-content: center;
 }
 
-.icons{
+.icons {
     display: flex;
     justify-content: space-between;
     margin: 5%;
 }
 
-    .central{
+.central {
     text-align: center;
+    width: 100%;
+    .img-card {
+        width: 90%;
+        height: 60%;
+        margin: 0 5% 20px 5%;
+        text-align: center;
+        overflow: hidden;
+        img {
+            display: inline-block;
+            width: auto;
+            height: 100%;
+        }
+    }
+    .card-body {
+        background: #006e99;
+        color: white;
         width: 100%;
-        .img-card{
-            width: 90%;
-            height: 60%;
-            margin: 0 5% 20px 5%;
-            text-align: center;
-            overflow:hidden;
-            img{
-                display: inline-block;
-                width: auto;
-                height: 100%;
-            }
-            
-        }
-        .card-body{
-            background: #006e99;
-            color: white;
-            width: 100%;
-            height: 20%;
-        }
-        .stars{
-            background: #e74c3c;
-            width: 100%;
-            height: 20%;
-        }
-   }
-
-.name {
-    background-color: #006e99;
+        height: 20%;
+    }
+    .stars {
+        background: #e74c3c;
+        width: 100%;
+        height: 20%;
+    }
 }
+
 
 .card {
+    &_favorited {
+        transform-origin: 0.5em 0.5em;
 
-  &_favorited {
-    transform-origin: 0.5em 0.5em;
-
-    &::after {
-      z-index: 1;
-      opacity: 1;
+        &::after {
+            z-index: 1;
+            opacity: 1;
+        }
     }
-  }
 
-  &_hidden {
-    display: none;
-  }
+    &_hidden {
+        display: none;
+    }
 }
 
-button{
+button {
     border: none;
 }
 </style>
